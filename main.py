b19ed6f948963062e48abade703532261df378a2
takehiro <songs.of.the.sword@gmail.com>
2020-03-29 00:35:47 +0900

    webhookの「検証」抜けtest

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+# def callback():
+ # get X-Line-Signature header value　残す
+ # signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
+ # get request body as text　残す
+    # body = request.get_data(as_text=True)
+    # app.logger.info("Request body: " + body)
+# 下四つ後で消す
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+    # try:
+        # handler.handle(body, signature)
+    # except InvalidSignatureError:
+        # abort(400)
 
-    return 'OK'
+    # return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
