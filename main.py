4fd588b3d4c9c49be3e41fed8fa8f74c1ac01407
takehiro <songs.of.the.sword@gmail.com>
2020-03-29 21:14:06 +0900

    setting の追加　コメントアウト

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def callback():
    # get request body as text
    body = request.get_data(as_text=True)
    
-   app.logger.info("Request body: " + body)
+#    app.logger.info("Request body: " + body) 消してみた
    
    # handle webhook body
    try:
