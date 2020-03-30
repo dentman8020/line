35a00291ec03b8242a97be8b4ea20c3b3113e125
takehiro <songs.of.the.sword@gmail.com>
2020-03-30 23:51:43 +0900

    req のflask

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-"""
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-# import gunicorn
-"""
-from flask import Flask, request, abort
-
-from linebot import  LineBotApi
+from linebot import LineBotApi
 from linebot import WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent
