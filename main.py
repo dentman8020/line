ca2120d8b3212c72238cc139bd9be3125228905c
takehiro <songs.of.the.sword@gmail.com>
2020-04-08 17:40:15 +0900

    てｓｔ

--- main.py
+++ main.py
@@ -52,10 +52,12 @@ def callback():
 
 #以下でWebhookから送られてきたイベントをどのように処理するかを記述する
 @handler.add(MessageEvent, message=TextMessage)
+"""
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+"""
 def message_text(event):
     if 'おはよう' in event.message.text:
         content = 'おはようございます'
